11d97745694c9282900dbcb58c9e4d56ca9a575a
dit03 <dit@imac-4.local>
2019-10-04 15:50:11 +0900

    2019.10.04

--- 03SimpleMap/ViewController.swift
+++ 03SimpleMap/ViewController.swift
@@ -13,65 +13,47 @@ class ViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
-    var points = [AnnotationData]()
+    var points = [MKPointAnnotation]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        //35.166537 위도Latitude, 129.072061 경도longitude (학교)
-        
+
         //중심점 설정
         let center = CLLocationCoordinate2D(latitude: 35.166537, longitude: 129.072061)
         
-        //반경 설정
-        //let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-        
-        //region 설정
-        //let region = MKCoordinateRegion(center: center, span: span)
-        //let region = MKCoordinateRegion(center: center, latitudinalMeters: 1000, longitudinalMeters: 1000)
-        
-        //MapView에 추가
-        //mapView.setRegion(region, animated: true)
-        
-        //MKMapViewDelegate
         mapView.delegate = self
         
+        let path = Bundle.main.path(forResource: "PinData", ofType: "plist")
+        //print(path)
         
-        //annotation set
-//        let ann = MKPointAnnotation()
-//        ann.coordinate = center
-//        ann.title = "동의과학대학교"
-//        ann.subtitle = "미래관"
-//        points.append(ann)
-        let ann1 = AnnotationData(coordinate: center, title: "동의과학대학교", subtitle: "미래관")
-        points.append(ann1)
+        let contents = NSArray(contentsOfFile: path!)
+        //print(contents)
         
-        //2
-//        let ann2 = MKPointAnnotation()
-//        ann2.coordinate.latitude = 35.161167
-//        ann2.coordinate.longitude = 129.064525
-//        ann2.title = "토포필리아센트럴"
-//        ann2.subtitle = "우리집"
-//        points.append(ann2)
-        let ann2 = AnnotationData(coordinate: CLLocationCoordinate2D(latitude: 35.161167, longitude: 129.064524), title: "토포필리아센트럴", subtitle: "우리집")
-        points.append(ann2)
+        if let myItem = contents {
+            for item in myItem {
+                let lat = (item as AnyObject).value(forKey: "lat")
+                let long = (item as AnyObject).value(forKey: "long")
+                let title = (item as AnyObject).value(forKey: "title")
+                let subTitle = (item as AnyObject).value(forKey: "subTitle")
+                
+                let myLat = (lat as! NSString).doubleValue
+                let myLong = (long as! NSString).doubleValue
+                
+                let anno = MKPointAnnotation()
+                anno.coordinate.latitude = myLat
+                anno.coordinate.longitude = myLong
+                anno.title = title as! String
+                anno.subtitle = subTitle as! String
+                
+                points.append(anno)
+                
+            }
+        } else {
+            print("Error")
+        }
         
-//        let ann3 = MKPointAnnotation()
-//        ann3.coordinate.longitude = 128.943159
-//        ann3.coordinate.latitude = 35.105746
-//        ann3.title = "을숙도"
-//        ann3.subtitle = "경치 좋은 영도 맛집"
-//        points.append(ann3)
-        let ann3 = AnnotationData(coordinate: CLLocationCoordinate2D(latitude: 35.105746, longitude: 128.943159), title: "을숙도", subtitle: "어디지")
-        points.append(ann3)
-        
-        //PointAnnotation add
-        //mapView.addAnnotations([ann, ann2])
-        //모든 핀을 다 보여줌
         mapView.showAnnotations(points, animated: true)
-        
-        //35.161167, 129.064525 (우리집)
     }
     
     
@@ -103,7 +85,6 @@ class ViewController: UIViewController, MKMapViewDelegate {
             
             if annotation.title == "동의과학대학교" {
                 annotationView?.pinTintColor = UIColor.blue
-                annotationView?.setSelected(true, animated: false)
             } else if annotation.title == "토포필리아센트럴" {
                 annotationView?.pinTintColor = UIColor.red
             } else if annotation.title == "을숙도" {
@@ -126,7 +107,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
     }
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-        let alertController = UIAlertController(title: "title", message: "sub", preferredStyle: .actionSheet)
+        let alertController = UIAlertController(title: (view.annotation?.title)!, message: (view.annotation?.subtitle)!, preferredStyle: .actionSheet)
         
         let action = UIAlertAction(title: "OK", style: .default)
         
